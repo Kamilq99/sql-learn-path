881c70509888c8cf0ff06e35e56c820bcdb2b1f8
Kamilq99 <thealti222@gmail.com>
2025-05-03 16:52:16 +0200

    Adding first data and showing in table this data

---
+++ First_tasks/add_data/add_data.sql
@@ -0,0 +1,16 @@
+USE TEST;
+
+INSERT INTO Users(FIRST_NAME, LAST_NAME, EMAIL, AGE)
+VALUES ('John', 'Doe', 'john_doe@example.com', 25);
+
+INSERT INTO Users(FIRST_NAME, LAST_NAME, EMAIL, AGE)
+VALUES ('Jane', 'Doe', 'jane_doe@example.com', 28);
+
+INSERT INTO Users(FIRST_NAME, LAST_NAME, EMAIL, AGE)
+VALUES ('Bob', 'Doe', 'bob_doe@example.com', 30);
+
+INSERT INTO Users(FIRST_NAME, LAST_NAME, EMAIL, AGE)
+VALUES ('Alice', 'Doe', 'alice_doe@example.com', 32);
+
+INSERT INTO Users(FIRST_NAME, LAST_NAME, EMAIL, AGE)
+VALUES ('Charlie', 'Doe', 'charlie_doe@example.com', 35);
