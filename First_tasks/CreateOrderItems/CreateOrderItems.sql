880392b7c8c6566c9b8394ed25e3c358eb402eb5
Kamilq99 <83961352+Kamilq99@users.noreply.github.com>
2025-05-05 10:25:04 +0200

    Adding OrderItems Table

---
+++ First_tasks/CreateOrderItems/CreateOrderItems.sql
@@ -0,0 +1,10 @@
+USE TEST;
+
+CREATE TABLE OrderItems (
+    OrderItemID INT IDENTITY(1,1) PRIMARY KEY,
+    OrderID INT,
+    ProductID INT,
+    Quantity INT,
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);
