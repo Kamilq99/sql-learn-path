8efae99ec279581866f66e158f0073952a28bb09
Kamilq99 <thealti222@gmail.com>
2026-01-12 19:46:18 +0100

    Add new rows

---
+++ TestIndexDB/addnewrows.sql
@@ -0,0 +1,10 @@
+INSERT INTO Dzialy (Nazwa)
+VALUES ('IT'), ('HR'), ('Finanse'), ('Marketing'), ('Sprzeda¿');
+
+INSERT INTO Stanowiska (Nazwa, Poziom)
+VALUES 
+('Junior', 1),
+('Mid', 2),
+('Senior', 3),
+('Manager', 4);
+GO
