172fc550fa1da3fe6abbdf8edb68925541fb1e29
Kamilq99 <thealti222@gmail.com>
2026-01-12 19:08:01 +0100

    Index Fragmentation

---
+++ TestIndexDB/showindexfragmentation.sql
@@ -0,0 +1,17 @@
+-- Sprawdzenie fragmentacji dla tabeli Pracownicy
+SELECT 
+    dbschemas.[name] AS SchemaName,
+    dbtables.[name] AS TableName,
+    dbindexes.[name] AS IndexName,
+    indexstats.avg_fragmentation_in_percent
+FROM 
+    sys.dm_db_index_physical_stats(DB_ID('TestDB'), OBJECT_ID('dbo.Pracownicy'), NULL, NULL, 'LIMITED') AS indexstats
+JOIN 
+    sys.tables dbtables ON dbtables.[object_id] = indexstats.[object_id]
+JOIN 
+    sys.schemas dbschemas ON dbtables.[schema_id] = dbschemas.[schema_id]
+JOIN 
+    sys.indexes AS dbindexes ON dbindexes.[object_id] = indexstats.[object_id]
+                             AND indexstats.index_id = dbindexes.index_id
+WHERE dbindexes.[name] IS NOT NULL;
+GO
