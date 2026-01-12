9637268eee6c919aec75d9f528e16aea11811796
Kamilq99 <thealti222@gmail.com>
2026-01-12 19:50:49 +0100

    New Data

--- TestIndexDB/showindexfragmentation.sql
+++ TestIndexDB/showindexfragmentation.sql
@@ -1,17 +1,10 @@
--- Sprawdzenie fragmentacji dla tabeli Pracownicy
-SELECT 
-    dbschemas.[name] AS SchemaName,
-    dbtables.[name] AS TableName,
-    dbindexes.[name] AS IndexName,
-    indexstats.avg_fragmentation_in_percent
-FROM 
-    sys.dm_db_index_physical_stats(DB_ID('TestDB'), OBJECT_ID('dbo.Pracownicy'), NULL, NULL, 'LIMITED') AS indexstats
-JOIN 
-    sys.tables dbtables ON dbtables.[object_id] = indexstats.[object_id]
-JOIN 
-    sys.schemas dbschemas ON dbtables.[schema_id] = dbschemas.[schema_id]
-JOIN 
-    sys.indexes AS dbindexes ON dbindexes.[object_id] = indexstats.[object_id]
-                             AND indexstats.index_id = dbindexes.index_id
-WHERE dbindexes.[name] IS NOT NULL;
-GO
+SELECT
+    OBJECT_NAME(ips.object_id) AS TableName,
+    i.name AS IndexName,
+    ips.avg_fragmentation_in_percent
+FROM sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, 'DETAILED') ips
+JOIN sys.indexes i
+    ON ips.object_id = i.object_id
+   AND ips.index_id = i.index_id
+WHERE i.name IS NOT NULL
+ORDER BY ips.avg_fragmentation_in_percent DESC;
